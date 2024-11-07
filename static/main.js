a56ead1e079cc5ae380f7f304012f2421593647a
mjgarin <mjgarin@git.ca>
2024-11-07 20:15:21 +0800

    updated ver

--- static/main.js
+++ static/main.js
@@ -11,3 +11,34 @@ function toggleSidebar() {
         mainContent.classList.add('active');
     }
 }
+    // Notificationo Bell 
+    function toggleNotification() {
+      const notificationContent = document.getElementById('notificationContent');
+      notificationContent.style.display = 
+        notificationContent.style.display === 'block' ? 'none' : 'block';
+    }
+
+    document.addEventListener('click', function(event) {
+      const notificationContent = document.getElementById('notificationContent');
+      const bell = document.querySelector('.notification');
+      if (!notificationContent.contains(event.target) && !bell.contains(event.target)) {
+        notificationContent.style.display = 'none';
+      }
+    });
+ 
+    // Menu Bar
+    function toggleMenu() {
+        const dropdown = document.getElementById("dropdownMenu");
+        dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+    }
+    
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.menu-button')) {
+            const dropdown = document.getElementById("dropdownMenu");
+            if (dropdown.style.display === "block") {
+                dropdown.style.display = "none";
+            }
+        }
+    }
+    
