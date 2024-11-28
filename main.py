74e3ad46cc4fac0d0322beb26fec7ee912b02f12
Akiroze <daryl.mike.d@gmail.com>
2024-11-28 20:02:18 +0800

    Fixed HTML Header structures, made the href dynamic and Added flask urls

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from website import create_app
+from flask import Flask, render_template
 
-app = create_app()
+# Initialize Flask app
+app = Flask(__name__, template_folder='templates')
 
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/forgot-password')
+def forgot_password():
+    return render_template('forgot_password.html')  # Or whatever template you wish to use
+
+@app.route('/userscreen')
+def userscreen():
+    return render_template('userscreen.html')  # Replace with your actual template or logic
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
